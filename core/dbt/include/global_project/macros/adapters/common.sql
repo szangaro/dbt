88125ba6608c1347329f93a10210130feb3ef57e
Andrew Seitz <andrew.seitz@snowflake.com>
2020-04-14 09:24:24 -0700

    adding in Drews feedback

--- core/dbt/include/global_project/macros/adapters/common.sql
+++ core/dbt/include/global_project/macros/adapters/common.sql
@@ -137,7 +137,8 @@
 {% endmacro %}
 
 {% macro default__alter_column_comment(relation, column_name, new_column_type) -%}
-  {{ log("This is not consistently implemented yet") }}
+  {{ exceptions.raise_not_implemented(
+    'alter_column_comment macro not implemented for adapter '+adapter.type()) }}
 {% endmacro %}
 
 {% macro alter_relation_comment(relation, relation_comment) -%}
@@ -145,7 +146,8 @@
 {% endmacro %}
 
 {% macro default__alter_relation_comment(relation, relation_comment) -%}
-  {{ log("This is not consistently implemented yet") }}
+  {{ exceptions.raise_not_implemented(
+    'alter_relation_comment macro not implemented for adapter '+adapter.type()) }}
 {% endmacro %}
 
 
